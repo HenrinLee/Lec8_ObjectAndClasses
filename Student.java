fe02c8886bc1a6e42cfc78b3bc77783ead726754
Trần Lê Văn Minh <minhtran020207@gmail.com>
2026-02-04 23:49:24 +0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,22 +1,17 @@
-package Lec8_ObjectAndClasses.Constructor.EX3;
+package Lec8_ObjectAndClasses.Constructor.EX2;
 
 public class Student {
 
     int id;
     String name;
 
-    Student(int i, String n) {
-        id = i;
-        name = n;
-    }
-
     void display() {
         System.out.println(id + " " + name);
     }
 
     public static void main(String[] args) {
-        Student s1 = new Student(111, "Karan");
-        Student s2 = new Student(222, "Aryan");
+        Student s1 = new Student();
+        Student s2 = new Student();
         s1.display();
         s2.display();
     }
